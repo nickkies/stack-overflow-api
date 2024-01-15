934e5d48829c73d1717b2120e5f798279578df32
YoonSung Kim <opqudk@gmail.com>
2024-01-15 15:15:41 +0900

    🚧 feat: add db connection

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,41 @@
 #[macro_use]
 extern crate rocket;
 
+extern crate log;
+
+extern crate pretty_env_logger;
+
 mod cors;
 mod handlers;
 mod models;
 
 use cors::*;
+use dotenvy::dotenv;
 use handlers::*;
+use sqlx::postgres::PgPoolOptions;
 
 #[launch]
-fn rocket() -> _ {
+async fn rocket() -> _ {
+    pretty_env_logger::init();
+    dotenv().ok();
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&std::env::var("DATABASE_URL").expect("DATABASE_URL must be set."))
+        .await
+        .expect("Failed to create Postgres connection pool!");
+
+    // TODO Remove
+    tokio::spawn(async move {
+        let questions = sqlx::query!("SELECT * FROM question")
+            .fetch_all(&pool)
+            .await
+            .expect("Failed to execute query.");
+
+        debug!("********* Question Records *********");
+        debug!("{questions:?}");
+    });
+
     rocket::build()
         .mount(
             "/",
