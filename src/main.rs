9406f40c7315d399811adf71dc75f868a06455ca
YoonSung Kim <opqudk@gmail.com>
2024-01-15 10:26:21 +0900

    ✨ feat: attach cors

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,26 @@
 #[macro_use]
 extern crate rocket;
 
+mod cors;
 mod handlers;
 mod models;
 
+use cors::*;
 use handlers::*;
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount(
-        "/",
-        routes![
-            create_question,
-            read_questions,
-            delete_question,
-            create_answer,
-            read_answers,
-            delete_answer
-        ],
-    )
+    rocket::build()
+        .mount(
+            "/",
+            routes![
+                create_question,
+                read_questions,
+                delete_question,
+                create_answer,
+                read_answers,
+                delete_answer
+            ],
+        )
+        .attach(CORS)
 }
