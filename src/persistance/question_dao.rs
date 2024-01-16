4218459887bc6821c9020827d4417b57ff21ec8c
YoonSung Kim <opqudk@gmail.com>
2024-01-16 13:49:04 +0900

    👔 feat: add create question dao

--- src/persistance/question_dao.rs
+++ src/persistance/question_dao.rs
@@ -1,3 +1,4 @@
+use async_trait::async_trait;
 use sqlx::PgPool;
 
 use crate::models::{DBError, Question, QuestionDetail, QuestionId};
@@ -21,8 +22,26 @@ impl QuestionDaoImpl {
 
 #[async_trait]
 impl QuestionDao for QuestionDaoImpl {
-    async fn create_question(&self, qustion: Question) -> Result<QuestionDetail, DBError> {
-        todo!();
+    async fn create_question(&self, question: Question) -> Result<QuestionDetail, DBError> {
+        let record = sqlx::query!(
+            r#"
+              INSERT INTO question ( title, description )
+              VALUES ( $1, $2 )
+              RETURNING *
+            "#,
+            question.title,
+            question.description
+        )
+        .fetch_one(&self.db)
+        .await
+        .map_err(|e| DBError::Other(Box::new(e)))?;
+
+        Ok(QuestionDetail {
+            question_uuid: record.question_uuid.to_string(),
+            title: record.title,
+            description: record.description,
+            created_at: record.created_at.to_string(),
+        })
     }
 
     async fn get_questions(&self) -> Result<Vec<QuestionDetail>, DBError> {
